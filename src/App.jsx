eea8161069c1595600e6c4a674c9747b4ba1a89d
yashG <yrg.creation.ai@gmail.com>
2025-09-20 19:22:08 +0530

    ading animation to vi text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { gsap } from 'gsap';
+import {useGSAP} from '@gsap/react';
+
 
 function App() {
+  
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.to(".vi-mask-group", {
+      rotate: 10,
+      duration: 2,
+      ease: "power4.easeInOut",
+      transformOrigin: "50% 50%",
+    })
+    .to(".vi-mask-group", {
+        scale: 10,
+        duration: 2,
+        delay: -1.8,
+        ease: "Expo.easeInOut",
+        transformOrigin: "50% 50%",
+        opacity: 0,
+    })
+  })
+
   return (
     <div>
       <div className="svg fixed top-0 left-0 z-[99] w-full h-screen overflow-hidden bg-[#000] ">
-            <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+           <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
                <defs>
                    <mask id="viMask">
                       <rect width="100%" height="100%" fill="black" />
@@ -29,7 +52,7 @@ function App() {
                     preserveAspectRatio="xMidYMid slice"
                     mask="url(#viMask)"
                 />
-            </svg>
+           </svg>
       </div>
     </div>
   )
