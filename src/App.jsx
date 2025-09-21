194e48f30bf124f364c098d4d55a505ba91b0a1d
yashG <yrg.creation.ai@gmail.com>
2025-09-21 17:54:43 +0530

    landing page done*

--- src/App.jsx
+++ src/App.jsx
@@ -79,6 +79,13 @@ function App() {
                <div className="imagesDiv relative w-full h-screen overflow-hidden">
                   <img src="./sky.png" alt="Image" className="absolute top-0 left-0 object-cover w-full h-full" />
                   <img src="./bg.png" alt="Image" className="absolute top-0 left-0 object-cover w-full h-full" />
+
+                  <div className="text-d flex flex-col gap-[25vh] text-white absolute top-[15%] left-1/2 -translate-x-1/2">
+                     <h1 className='text-[8rem] uppercase font-[900] leading-0 -ml-5'>grand</h1>
+                     <h1 className='text-[8rem] uppercase font-[900] leading-0 ml-70'>theft</h1>
+                     <h1 className='text-[8rem] uppercase font-[900] leading-0 -ml-5'>auto</h1>
+                  </div>
+
                   <img src="./girlbg.png" alt="Image" className="Gimg absolute top-[10%] left-1/2 -translate-x-1/2 w-[40vw] " />
                </div>
                <div className="btmBar w-full py-4 absolute bottom-0 left-0 bg-gradient-to-t from-black to-transparent pl-5">
